a1b4abfe2e77ef8e2daa3ecf88fd7d505b2c670c
Jake Runzer <jakerunzer@gmail.com>
2016-05-07 01:35:02 -0700

    change to multi team slack app

--- index.js
+++ index.js
@@ -1,51 +1,34 @@
 'use strict'
 
-var restify = require('restify');
-var logger = require('morgan');
+// Beep Boop specifies the port you should listen on default to 8080 for local dev
+var PORT = process.env.PORT || 8080
 
-var VERIFY_TOKEN = process.env.SLACK_VERIFY_TOKEN;
-if (!VERIFY_TOKEN) {
-    console.error('SLACK_VERIFY_TOKEN is required');
-    process.exit(1);
-}
-// Use Beep Boop provided PORT - default to 8080 for dev
-var PORT = process.env.PORT || 8080;
+var controller = botkit.slackbot()
 
-var server = restify.createServer();
-server.use(logger('tiny'));
+require('beepboop-botkit').start(controller, {
+    debug: true
+})
 
-// Add a `/beepboop` route handler for `/beepboop` slash command
-server.post('/fakt', restify.bodyParser(), function(req, res) {
-    if (req.params.token !== VERIFY_TOKEN) {
-        return res.send(401, 'Unauthorized')
-    }
-
-    var message = 'boopbeep';
-
-    // Handle any help requests
-    if (req.params.text === 'help') {
-        message = "Sorry, I can't offer much help, just here to beep and boop";
-    }
-
-    res.send({
-        response_type: 'ephemeral',
-        text: message
-    });
-});
-
-// Add a GET handler for `/beepboop` route that Slack expects to be present
-server.get('/fakt', function(req, res) {
-    res.send(200, 'Ok');
-});
-
-// 🔥 it up
-server.listen(PORT, function(err) {
+controller.setupWebserver(PORT, function(err, webserver) {
     if (err) {
-        return console.error('Error starting server: ', err);
+        console.error(err)
+        process.exit(1)
     }
 
-    console.log('Server successfully started on port %s', PORT);
-});
+    webserver.use(logger('tiny'))
+        // Setup our slash command webhook endpoints
+    controller.createWebhookEndpoints(webserver)
+})
+
+controller.on('slash_command', function(bot, message) {
+    switch (message.command) {
+        case '/fakt':
+            bot.reply(message, 'lkhlkhklh')
+            break
+        default:
+            bot.replyPrivate(message, "Sorry, I'm not sure what that command is")
+    }
+})
 
 var Wiki = require('./wiki/wiki.js');
 
